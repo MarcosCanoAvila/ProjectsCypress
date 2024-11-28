683643a5335e2fe7d7319e0c597387571870a416
marcos cano <canoavilamarcos@gmail.com>
2024-11-27 22:47:24 -0600

    Actualizacion de la primera clase

--- cypress/e2e/3-integration/a-seccion-one/Hola_mundo.cy.js
+++ cypress/e2e/3-integration/a-seccion-one/Hola_mundo.cy.js
@@ -1,7 +1,14 @@
 describe("Bienvenido al curso de cypress sección 1", () =>{
     it('Mi primer test -> el famoso hola mundo', () => {
         cy.log("Hola mundo");
-        
+        cy.wait(1000);
     });
-  } )//Cierre de describe
+
+    it("Mi segundo test -> Campo name", () =>{
+        cy.visit("http://www.uitestingplayground.com/textinput");
+        cy.get("#newButtonName").type("Marcos Cano");
+        cy.wait(1000);
+    })
+
+  })//Cierre de describe
   
