34a51834139bb687ed849c25512ceb0c533f03fb
marcos cano <canoavilamarcos@gmail.com>
2024-11-27 20:34:53 -0600

    Actualización

---
+++ cypress/e2e/2-advanced-examples/Hola_mundo.js
@@ -0,0 +1,6 @@
+describe("Bienvenido al curso de cypress sección 1", () =>{
+    it('Mi primer test -> el famoso hola mundo', () => {
+        cy.console.log("Hola mundo");
+        
+    });
+} )//Cierre de describe
