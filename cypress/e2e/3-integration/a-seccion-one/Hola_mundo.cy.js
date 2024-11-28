de70991aa6a7bff1ef8ca434b719e67ee143317b
marcos cano <canoavilamarcos@gmail.com>
2024-11-27 21:33:28 -0600

    Correccion de archivo

--- cypress/e2e/3-integration/a-seccion-one/Hola_mundo.cy.js
+++ cypress/e2e/3-integration/a-seccion-one/Hola_mundo.cy.js
@@ -1,6 +1,7 @@
 describe("Bienvenido al curso de cypress sección 1", () =>{
     it('Mi primer test -> el famoso hola mundo', () => {
-        cy.console.log("Hola mundo");
+        cy.log("Hola mundo");
         
     });
-} )//Cierre de describe
+  } )//Cierre de describe
+  
--- cypress/e2e/spec.cy.js
+++
@@ -1,6 +0,0 @@
-describe("Bienvenido al curso de cypress sección 1", () =>{
-  it('Mi primer test -> el famoso hola mundo', () => {
-      cy.log("Hola mundo");
-      
-  });
-} )//Cierre de describe
